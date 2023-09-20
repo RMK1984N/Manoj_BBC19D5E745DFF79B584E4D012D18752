dc1e961de5750c1da3364dd271fcc2b89962ae3e
RMK1984N <144201472+RMK1984N@users.noreply.github.com>
2023-09-20 12:35:55 +0530

    Delete Unit-2 challenge 2 directory

--- Unit-2 challenge 2/main.py
+++
@@ -1,9 +0,0 @@
-# check the factorial number using recursive function
-def fact(n):
-  if(n==0 or n==1):
-    return 1
-  else:
-    return n*fact(n-1)
-n=int(input ("Enter the number : "))
-res=fact(n)
-print("find the result will be factorial number is :",res)
